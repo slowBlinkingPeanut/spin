d4be641b198623ef8dca3538ca316681676783cb
slowBlinkingPeanut <110095190+slowBlinkingPeanut@users.noreply.github.com>
2022-07-28 10:05:47 -0500

    Add files via upload

---
+++ Sprite.gd
@@ -0,0 +1,22 @@
+extends Sprite
+
+var screen_size 
+var angular_speed = PI
+var speed = 200.0
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var direction = 0
+	if Input.is_mouse_button_pressed(1):
+		direction = -1
+	else:
+		direction = 1
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
